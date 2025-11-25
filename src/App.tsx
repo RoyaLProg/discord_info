8d86a27e12f3e25c36c3ed36c463467dfaa5dedb
Camille CAMBIUM <camille.cambium@protonmail.com>
2025-11-25 05:58:56 +0100

    feat: discord_info

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import Background from './assets/background.mp4'
+
+function App() {
+	return (
+		<div className='bg-black w-screen h-screen overflow-hidden'>
+			<div className='sm:block md:hidden w-full h-full text-white flex justify-center items-center'>
+				<p> fuck your phone screen </p>
+			</div>
+			<video src={Background} controls={false} autoPlay className='size-full z-1' loop/>
+			<div className='h-screen w-screen -translate-y-full flex items-center justify-center'>
+				<div className='h-1/2 w-1/2 z-100 flex flex-col justify-center p-4 gap-4'>
+					<div className='flex gap-4 items-center relative'>
+						<div className='w-32 h-32 overflow-hidden'>
+							<img src="https://cdn.discordapp.com/avatars/320302900553318400/116f123f99566cf588e624e6e27b6c6f.webp?size=512" className='rounded-full'/>
+							<img src="https://cdn.discordapp.com/avatar-decoration-presets/a_c48b135704ecb5c88f2f71f6c8bcce2f.png" className='-translate-y-full'/>
+						</div>
+						<p className='text-white text-5xl font-extrabold text-center'>RoyaL</p>
+						<div className='bg-red-400/50 p-2 rounded-xl flex items-center gap-2 h-12'>
+							<img src="https://cdn3.emoji.gg/emojis/98152-pink-heart-guild-tag.png" width='32px' alt="Pink_Heart_Guild_Tag"/>
+							<p className='font-bold'>TETO</p>
+						</div>
+					</div>
+					<div className='w-full h-1/3 flex gap-2'>
+						<div className='w-1/2 bg-gray-500/30 backdrop-blur rounded-xl flex items-center'>
+							<p className='text-center font-bold w-full text-gray-200'>INCOMING DISCORD</p>
+						</div>
+						<div className='w-1/2 bg-gray-500/30 backdrop-blur rounded-xl flex items-center'>
+							<p className='text-center font-bold w-full text-gray-200'>INCOMING PORTFOLIO</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
